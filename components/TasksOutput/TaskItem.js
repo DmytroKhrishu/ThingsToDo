6f42d488af9475f3251adf3ccd33c82af4fa45d6
Dmytro Khrishu <dima.khrishu@gmail.com>
2024-05-10 13:24:16 +0300

    trimming description in TaskItem

--- components/TasksOutput/TaskItem.js
+++ components/TasksOutput/TaskItem.js
@@ -2,6 +2,13 @@ import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { Colors } from '../../const/colors';
 
 export default function TaskItem({ task, onClick }) {
+  let taskDescription = '';
+  if (task.description.length >= 25) {
+    taskDescription = task.description.substring(0, 25) + '...';
+  } else {
+    taskDescription = task.description;
+  }
+
   return (
     <View style={styles.itemContainer}>
       <Pressable
@@ -11,7 +18,7 @@ export default function TaskItem({ task, onClick }) {
       >
         <Text style={styles.title}>{task.task}</Text>
         <View style={styles.detailsContainer}>
-          <Text style={styles.text}>{task.description}</Text>
+          <Text style={styles.text}>{taskDescription}</Text>
           <Text style={styles.text}>{task.date}</Text>
         </View>
       </Pressable>
@@ -36,14 +43,14 @@ const styles = StyleSheet.create({
     padding: 5,
     color: 'white',
     fontSize: 16,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   detailsContainer: {
-    flexDirection: "row",
+    flexDirection: 'row',
     justifyContent: 'space-between',
-    paddingHorizontal: 8
+    paddingHorizontal: 8,
   },
   text: {
-    color: "white"
-  }
+    color: 'white',
+  },
 });
